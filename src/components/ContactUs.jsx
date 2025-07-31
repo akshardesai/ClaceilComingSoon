53a4f45c84af4e85edcfb0ae59ed16837c044ad2
AksharDesai <akshar25desai@gmail,com>
2025-07-31 13:43:20 +0530

    dribble clone complete

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+
+    <div className="border-2  w-[35%] flex mt-4">
+    <div className="button-container  w-[85%]">
+
+    <button class="text-gray-400 text-[12px] p-1">Enter Your Email here  </button>
+    </div>
+    <div className="icon-container bg-black w-[25%] flex justify-center">
+
+    <i class="ri-arrow-right-line text-white "></i>
+    </div>
+    </div>
+  )
+}
+
+export default ContactUs
